22bf1384675a06304ae4c1ebad6bd0904ed1efcc
Lucile Grandjean <littlebip.lg@hotmail.fr>
2023-06-13 12:48:06 +0200

    Created route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+  end
+
+  def search
+    @users = User.all
+    @photos = Photos.all
+    # implement multi-search
   end
 end
