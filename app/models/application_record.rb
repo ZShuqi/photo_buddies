f340a177c2198b04bbd8cd503e9fe729b383f8b5
Lucile Grandjean <littlebip.lg@hotmail.fr>
2023-06-13 17:21:41 +0200

    Created search

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  self.abstract_class = true
+  PgSearch.multisearch_options = {
+    :using => {
+      :tsearch => {:prefix => true, :dictionary => "english"}
+    }
+  }
 end
