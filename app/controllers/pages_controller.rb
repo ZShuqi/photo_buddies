08636747e54e6c59a24ee11d7dbc4503fad9f190
Ayub Farah <ayubfarahh@gmail.com>
2023-06-13 16:50:48 +0100

    created profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def show
+    @user = User.find(params[:id])
+    @galleries = Gallery.all
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :comments
   has_many :events
   has_many :bookings
+  validates :username, presence: true, uniqueness: true
 end
