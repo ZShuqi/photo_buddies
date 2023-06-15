0435d8507152015cce6771d330b41cb6caa0860d
Ayub Farah <ayubfarahh@gmail.com>
2023-06-15 16:25:04 +0100

    profile page

--- app/policies/gallery_policy.rb
+++ app/policies/gallery_policy.rb
@@ -1,11 +1,12 @@
 class GalleryPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
-    # def show?
-    #   true
-    # end
+    def resolve
+      scope.all
+    end
+
+    def show?
+      true
+    end
   end
 end
