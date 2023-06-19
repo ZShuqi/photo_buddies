258e96a3bee5c72992f2db27b18c7382ddf08b40
Ayub Farah <ayubfarahh@gmail.com>
2023-06-19 16:07:48 +0100

    delete gallery

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :gallery
   has_many :comments, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
   has_one_attached :photo
 
