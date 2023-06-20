a3b6e592a3d66728d3c65164949b0ab615aba740
Shuqi Zheng <shuqizheng6@gmail.com>
2023-06-20 04:28:47 +0100

    test3 set tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :description, :email, :password, :password_confirmation, :photo ) }
 
