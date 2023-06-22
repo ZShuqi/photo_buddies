3a0e191c672e518522f6be0a0edb43b0b7e1f9c4
Shuqi Zheng <shuqizheng6@gmail.com>
2023-06-22 08:23:03 +0100

    halfway event map

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,7 @@ class Event < ApplicationRecord
   has_many_attached :images
 
   validates :title, :date, :description, :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
